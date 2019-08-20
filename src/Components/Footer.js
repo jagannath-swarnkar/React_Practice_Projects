c6f8b752d2d0db74ca7ca6415027f885e804b5d5
jagan <43448819+jagannath-swarnkar@users.noreply.github.com>
2019-08-20 21:23:20 +0530

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style/Footer.css';
+
+
+export class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+            </div>
+        )
+    }
+}
+
+export default Footer
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './style/Header.css';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h1>Welcome</h1>
+            </div>
+        )
+    }
+}
